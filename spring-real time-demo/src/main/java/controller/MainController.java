1de2fa306ad2e6733abb9df7dce1a545a14aa5b7
Abebe Adamu <abbkassa@gmail.com>
2019-06-21 23:49:42 -0400

    realtime ui update

--- spring-real time-demo/src/main/java/controller/MainController.java
+++ spring-real time-demo/src/main/java/controller/MainController.java
@@ -3,17 +3,17 @@ package controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import model.Menu;
 import service.MenuService;
-@RestController
+@Controller
 
 public class MainController {
 	private static List<Menu>menu=new ArrayList<Menu>();
@@ -68,8 +68,7 @@ public class MainController {
         menuService.sendMenu(menu);
         return new ModelAndView("order", "menu", menu);
 	}
-   
-	
+  
     
 }
 	
